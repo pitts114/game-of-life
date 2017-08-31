e2c5cfa1d61367fb8beae32f16d647cb60a944d4
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-08-31 15:21:15 -0400

    Add sim logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,84 @@
 import React, { Component } from 'react';
+import {Button} from 'react-bootstrap'
+import Board from './Board.js'
+
+const gridSize = 40
+
+const lowSpeed = 1000 //sim step time, ms
+const medSpeed = 500
+const highSpeed = 200
+
 
 class App extends Component {
+  constructor(){
+    super()
+
+    var arr = createCellArr()
+
+    this.state={
+      cellArr: arr,
+      simSpeed: medSpeed,
+      intervalId: undefined
+    }
+    this.startSim = this.startSim.bind(this)
+    this.pauseSim = this.pauseSim.bind(this)
+    this.clearSim = this.clearSim.bind(this)
+    this.processCells = this.processCells.bind(this)
+    this.setSimSpeed = this.setSimSpeed.bind(this)
+  }
+
+  startSim() {
+    if (!this.state.intervalId) {
+      var intervalId = setInterval(this.processCells, this.state.simSpeed)
+      const state = this.state
+      state.intervalId = intervalId
+      this.setState(state)
+    }
+  }
+
+  pauseSim() {
+    clearInterval(this.state.intervalId)
+    const state = this.state
+    state.intervalId = undefined
+    this.setState(state)
+  }
+
+  clearSim() {
+    const state = this.state
+    state.cellArr = createCellArr()
+    if (state.intervalId){
+      clearInterval(state.intervalId)
+      state.intervalId = undefined
+    }
+    this.setState(state)
+  }
+
+  processCells() {
+
+  }
+
+  setSimSpeed(timems) {
+    var state = this.state
+    state.simSpeed = timems
+    this.setState(state)
+  }
 
   render() {
     return (
-      <h1>Helo World!</h1>
+      <div className="container">
+        <Button>Run</Button><Button>Pause</Button><Button>Clear</Button>
+        <Board cellArr={this.state.cellArr} />
+      </div>
     );
   }
+}
+
+function createCellArr() {
+  var arr = new Array(gridSize)
+  for (var i = 0; i < gridSize; i++){
+    arr[i] = new Array(gridSize)
+  }
+  return arr
 }
 
 export default App;
--- src/Board.js
+++ src/Board.js
@@ -1,1 +1,36 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react'
+
+class Board extends Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return (
+      <h1>Hello World!</h1>
+    )
+  }
+}
+
+class Cell extends Component {
+  constructor() {
+    super()
+    this.state= {
+      age: 0
+    }
+    this.getOlder = this.getOlder.bind(this)
+  }
+
+  getOlder() {
+    var state = this.state
+    state.age = state.age + 1
+    this.setState(state)
+  }
+
+  render() {
+      var cell = <div className={'age' + this.state.age}></div>
+      this.getOlder()
+      return( {cell} )
+  }
+}
+
+export default Board
